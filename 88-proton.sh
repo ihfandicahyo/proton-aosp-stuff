31034c6de6c54e56b5e0b99d57c2e1e33b01511c
Ihfandi <39288119+ihfandicahyo@users.noreply.github.com>
2023-11-26 17:02:20 +0700

    Add files via upload

---
+++ 88-proton.sh
@@ -0,0 +1,50 @@
+#!/sbin/sh
+#
+# ADDOND_VERSION=2
+#
+# /system/addon.d/88-proton.sh
+# During a LineageOS upgrade, this script backs up proton,
+# /system is formatted and reinstalled, then the files are restored.
+#
+
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+etc/fonts.xml
+fonts/FiraCode-Regular.ttf
+fonts/InterVariable-Italic.ttf
+fonts/InterVariable.ttf
+fonts/SourceSerif4-Bold.ttf
+fonts/SourceSerif4-BoldIt.ttf
+fonts/SourceSerif4-It.ttf
+fonts/SourceSerif4-Regular.ttf
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/"$FILE"
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/"$FILE" "$R"
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    # Stub
+  ;;
+esac
